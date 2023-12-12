1a0bc6138583cf96583102082e03787b5831e0a1
AG <a@b.com>
2023-12-12 13:41:04 -0800

    Passing fourth test

--- test.py
+++ test.py
@@ -64,4 +64,34 @@ class TestIngest(unittest.TestCase):
         os.remove(new_file_path)
 
     def test_prefers_python_file_over_yaml_file(self):
-        self.assertEqual(1, 1)
+        flux = Flux()
+        # Create a temporary yaml file called cartridge.yaml
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".yaml") as temp_file:
+          yaml = YAML(typ="safe", pure=True)
+          yaml_data = {'a': [1, 2]}
+          yaml.dump(yaml_data, temp_file)
+
+          self.assertTrue(os.path.exists(temp_file.name))
+
+          # Move the temporary file to the current directory and rename to cartridge.yaml
+          new_file_path_yaml = os.path.join(os.path.dirname(__file__), "cartridge.yaml")
+          os.rename(temp_file.name, new_file_path_yaml)
+
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".py") as temp_file:
+          # Add some contents to temp_file
+          file_contents = b"cartridge = 'foo'"
+          temp_file.write(file_contents)
+          temp_file.close()
+
+          self.assertTrue(os.path.exists(temp_file.name))
+
+          # Move the temporary file to the current directory and rename to cartridge.yaml
+          new_file_path = os.path.join(os.path.dirname(__file__), "cartridge.py")
+          os.rename(temp_file.name, new_file_path)
+
+          found_cartridge = flux.find_cartridge()
+          self.assertEqual(found_cartridge, 'foo')
+
+
+        os.remove(new_file_path_yaml)
+        os.remove(new_file_path)
