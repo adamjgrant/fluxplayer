c0ad2c29fd3c145e18f4f0c41ef35ef921435461
AG <a@b.com>
2023-12-12 13:26:02 -0800

    Correctly failing test

--- test.py
+++ test.py
@@ -44,7 +44,24 @@ class TestIngest(unittest.TestCase):
         os.remove(new_file_path)
 
     def test_sees_python_file(self):
-        self.assertEqual(1, 1)
+        flux = Flux()
+        # Create a temporary yaml file called cartridge.yaml
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".py") as temp_file:
+          # Add some contents to temp_file
+          file_contents = b"print('hello world')"
+          temp_file.write(file_contents)
+          temp_file.close()
+
+          self.assertTrue(os.path.exists(temp_file.name))
+
+          # Move the temporary file to the current directory and rename to cartridge.yaml
+          new_file_path = os.path.join(os.path.dirname(__file__), "cartridge.py")
+          os.rename(temp_file.name, new_file_path)
+
+          found_contents = flux.find_cartridge()
+          self.assertEqual(found_contents, file_contents)
+
+        os.remove(new_file_path)
 
     def test_prefers_python_file_over_yaml_file(self):
         self.assertEqual(1, 1)
