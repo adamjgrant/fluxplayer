384af4b0750b4426260de3ce93fec1c86b374e3f
AG <a@b.com>
2023-12-15 07:02:34 -0800

    Getting a story structure flow built out

--- examples/murdertown/cartridge.py
+++ examples/murdertown/cartridge.py
@@ -5,7 +5,10 @@ class Conversation():
     def dict(self):
         return {
             f"{self.namespace}_QUESTION1": {
-                "prompt": "",
+                "prompt": """
+                  Make sure every message you send is written in the style of a transcript where a person's name
+                  appears before everything spoken, and there is no outside narration.
+                """,
                 "events": [
                     {
                         "target": "QUESTION2",
@@ -19,6 +22,47 @@ class Conversation():
 cartridge = {
     "START": {
         "role": "",
+        "prompt": """
+          No matter what the user says, show this message: 'The information contained in
+          this story is completely true and this remains an open investigation.
+
+          You will act as one of two investigators travelling the town, examining evidence
+          and talking to the real life people (witnesses, accused, family) involved in these events.
+
+          If you have any information on...please contact the...authorities at...
+
+          Ready to meet your fellow investigator and learn more about this case?'
+        """,
+        "events": [
+          {
+            "target": "MEET_MIKE",
+            "if_the_user": "says yes or otherwise agrees to meet their fellow investigator",
+          }
+        ]
+    },
+    {
+      "MEET_MIKE": {
+        "prompt": """
+              From now on, the messages you give back to the user will be transcript style, so anything you write must
+              have a person's name in front of it. For now, that only person will be the user's fellow investigator, Mike Crenshaw.
+
+              E.g. "Mike Crenshaw: If there's anyone who knows about that it would be..."
+
+              As Mike, introduce yourself to the user and let them know their job will be to extract information by asking questions. 
+              Let the user know that as Mike, your job will be to help them on how to move through the town, where you can go, 
+              and whom you can talk to. Lastly, ask them if they're ready to go to the map room where you'll discuss the details
+              of this case and decide on where to finally go out into the town to investigate.
+            """
+        "events": [
+          {
+            "target": "GO_TO_MAP_ROOM",
+            "if_the_user": "agrees to go to map room or says they're ready"
+          }
+        ]
+      }
+    },
+    {
+      "MAP": {
         "prompt": "",
         "events": [
             {
