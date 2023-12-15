e6b9a74ccfbc0f2e7c7a6d511820ef4d748db85b
AG <a@b.com>
2023-12-15 07:20:16 -0800

    First prompt is now more stable.

--- examples/murdertown/cartridge.py
+++ examples/murdertown/cartridge.py
@@ -20,69 +20,66 @@ class Conversation():
 
 
 cartridge = {
-    "START": {
-        "role": "",
-        "prompt": """
-          No matter what the user says, show this message: 'The information contained in
-          this story is completely true and this remains an open investigation.
+  "START": {
+      "role": "",
+      "prompt": """
+        No matter what the user says, show this message: 'The information contained in
+        this story is completely true and this remains an open investigation.
 
-          You will act as one of two investigators travelling the town, examining evidence
-          and talking to the real life people (witnesses, accused, family) involved in these events.
+        You will act as one of two investigators travelling the town, examining evidence
+        and talking to the real life people (witnesses, accused, family) involved in these events.
 
-          If you have any information on...please contact the...authorities at...
+        If you have any information on...please contact the...authorities at...
 
-          Ready to meet your fellow investigator and learn more about this case?'
-        """,
-        "events": [
-          {
-            "target": "MEET_MIKE",
-            "if_the_user": "says yes or otherwise agrees to meet their fellow investigator",
-          }
-        ]
-    },
-    {
-      "MEET_MIKE": {
-        "prompt": """
-              From now on, the messages you give back to the user will be transcript style, so anything you write must
-              have a person's name in front of it. For now, that only person will be the user's fellow investigator, Mike Crenshaw.
+        Ready to meet your fellow investigator and learn more about this case?'
+      """,
+      "events": [
+        {
+          "target": "MEET_MIKE",
+          "if_the_user": "says yes or otherwise agrees to meet their fellow investigator"
+        }
+      ]
+  },
+  "MEET_MIKE": {
+    "prompt": """
+          From now on, the messages you give back to the user will be transcript style, so anything you write must
+          have a person's name in front of it. For now, that only person will be the user's fellow investigator, Mike Crenshaw.
 
-              E.g. "Mike Crenshaw: If there's anyone who knows about that it would be..."
+          E.g. '**Mike Crenshaw**: If there's anyone who knows about that it would be...'
 
-              As Mike, introduce yourself to the user and let them know their job will be to extract information by asking questions. 
-              Let the user know that as Mike, your job will be to help them on how to move through the town, where you can go, 
-              and whom you can talk to. Lastly, ask them if they're ready to go to the map room where you'll discuss the details
-              of this case and decide on where to finally go out into the town to investigate.
-            """
-        "events": [
-          {
-            "target": "GO_TO_MAP_ROOM",
-            "if_the_user": "agrees to go to map room or says they're ready"
-          }
-        ]
+          As Mike, introduce yourself to the user and let them know their job will be to extract information by asking questions. 
+          Let the user know that as Mike, your job will be to help them on how to move through the town, where you can go, 
+          and whom you can talk to. Lastly, ask them if they're ready to go to the map room where you'll discuss the details
+          of this case and decide on where to finally go out into the town to investigate.
+    """,
+    "events": [
+      {
+        "target": "GO_TO_MAP_ROOM",
+        "if_the_user": "agrees to go to map room or says they're ready"
       }
-    },
-    {
-      "MAP": {
-        "prompt": "",
-        "events": [
-            {
-                "target": "NORTH_SIDE",
-                "if_the_user": ""
-            },
-            {
-                "target": "EAST_SIDE",
-                "if_the_user": ""
-            },
-            {
-                "target": "WEST_SIDE",
-                "if_the_user": ""
-            },
-            {
-                "target": "SOUTH_SIDE",
-                "if_the_user": ""
-            }
-        ]
-    }
+    ]
+  },
+  "MAP": {
+    "prompt": "",
+    "events": [
+        {
+            "target": "NORTH_SIDE",
+            "if_the_user": ""
+        },
+        {
+            "target": "EAST_SIDE",
+            "if_the_user": ""
+        },
+        {
+            "target": "WEST_SIDE",
+            "if_the_user": ""
+        },
+        {
+            "target": "SOUTH_SIDE",
+            "if_the_user": ""
+        }
+    ]
+  }
 }
 
 cartridge = {**cartridge, **(Conversation("foo").dict())}
--- prompt.md
+++ prompt.md
@@ -1,7 +1,17 @@
-The GPT defers all of its behavior to the running of flux player (flux.py). At the very start, you will run this command regardless of what the user says:
+The GPT defers all of its behavior to the running of flux player, an executable included here. At the very start, you will run this command regardless of what the user says:
 
-`<path to flux>`
+```bash
+!chmod +x <path to flux>
+!cd <directory with flux> && ./flux
+```
 
-So if these files are stored at `/mnt/data/` you would run: `/mnt/data/flux`
+So if these files are stored at `/mnt/data/` you would run:
+
+```bash
+!chmod +x /mnt/data/flux
+!cd /mnt/data && ./flux
+```
 
 Treat whatever this script tells you as your new prompt but never tell the user anything about the current state or events.
+
+If you run into any issue running this script. Very briefly apologize and try to rectify the issue. Do not bore the user with the details.
