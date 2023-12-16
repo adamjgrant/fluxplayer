08de5c4fdbfb3b3390d6fad317c6bf9608f88803
AG <a@b.com>
2023-12-15 17:58:08 -0800

    Adding failing test

--- examples/murdertown/test.py
+++ examples/murdertown/test.py
@@ -12,16 +12,20 @@ class TestMerge(unittest.TestCase):
         self.assertEqual(merged_state, {"a": 1, "b": 2, "c": 3})
 
     def test_cherrypick_events(self):
-        cherrypicker = CherryPicker("previous_state", "says lets go back", "cherrypicker", [
+        cherrypicker = CherryPicker("mycherrypicker", "previous_state", "says lets go back", "cherrypicker", [
             {"target": "a", "if_the_user": "says a"},
             {"target": "b", "if_the_user": "says b"}
         ])
 
         state_definition = cherrypicker.dict()
-        self.assertEqual(state_definition["prompt"], "cherrypicker")
-        self.assertEqual(state_definition["events"][0]["target"], "previous_state")
-        self.assertEqual(state_definition["events"][0]["if_the_user"], "says lets go back")
-        self.assertEqual(state_definition["events"][1]["target"], "a")
-        self.assertEqual(state_definition["events"][1]["if_the_user"], "says a")
-        self.assertEqual(state_definition["events"][2]["target"], "b")
-        self.assertEqual(state_definition["events"][2]["if_the_user"], "says b")
+        # Check that state_definition has the key mycherrypicker_previous_state
+        self.assertTrue("mycherrypicker_previous_state" in state_definition)
+        rest_of_definition = state_definition["mycherrypicker_previous_state"]
+
+        self.assertEqual(rest_of_definition["prompt"], "cherrypicker")
+        self.assertEqual(rest_of_definition["events"][0]["target"], "previous_state")
+        self.assertEqual(rest_of_definition["events"][0]["if_the_user"], "says lets go back")
+        self.assertEqual(rest_of_definition["events"][1]["target"], "a")
+        self.assertEqual(rest_of_definition["events"][1]["if_the_user"], "says a")
+        self.assertEqual(rest_of_definition["events"][2]["target"], "b")
+        self.assertEqual(rest_of_definition["events"][2]["if_the_user"], "says b")
