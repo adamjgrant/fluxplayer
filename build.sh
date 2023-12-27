9648b4d15239d0e3396d224d0d0db5f6588afa21
AG <a@b.com>
2023-12-27 12:30:08 -0800

    Saving

--- build.sh
+++ build.sh
@@ -1,14 +1,27 @@
 #!/bin/zsh
 
+RELEASE="2.0.3"
+
+rm -rf dist
+mkdir -p dist
+mkdir -p dist/openai
+mkdir -p dist/linux
+
 echo "Generating cartridges..."
 python3 flux.py -c examples/find_maura_murray/cartridge.py -x
 
-echo "Building project for Linux..."
+echo "Building project for Linux/OpenAI..."
 docker run -v "$(pwd):/src/" cdrx/pyinstaller-linux
+cp -r dist/linux/flux dist/openai/flux
+
+echo "Publishing GitHub release..."
+gh release create v$RELEASE ./dist/
 
 echo "Build completed."
 
 echo "Building for pip distribution..."
-python3 -m build -o dist/pip/
+python3 -m build -o dist/pip/*
 echo "Publishing pip distribution..."
 python3 -m twine upload --repository testpypi dist/pip/*
+
+# python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps flux-player-adamjgrant
---
+++ install.sh
@@ -0,0 +1,5 @@
+#! /bin/zsh
+
+brew install gh
+gh auth login
+pip install -r requirements.txt
